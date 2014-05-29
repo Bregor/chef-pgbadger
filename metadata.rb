93c73120ea6ab28012b493cd7374af8a788cffb8
Maxim Filatov <pipopolam@gmail.com>
2014-05-29 15:51:34 +0400

    Metadata

---
+++ metadata.rb
@@ -0,0 +1,9 @@
+name             'pgbadger'
+maintainer       'Evil Martians'
+maintainer_email 'bregor@evilmartians.com'
+license          'MIT'
+description      'Installs/Configures pgbadger'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.org'))
+version          '0.0.1'
+supports         'ubuntu'
+depends          'cron'
